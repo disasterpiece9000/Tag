4dcb8ef85d10b75f48a9507ddce462b4a674ef0a
James Grusheski <jamesrgrush@gmail.com>
2019-02-25 17:03:45 -0500

    Delete merge.py

--- merge.py
+++
@@ -1,7 +0,0 @@
-from tinydb import TinyDB
-
-opt = TinyDB("opt-in.json")
-new = TinyDB("player_data.json")
-
-for username in opt:
-	new.insert({"username": username['username'], "last_round": "", "score": 0})
